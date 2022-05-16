bc1fd46dbb4eb6453c805b8ff3d33da582b12d1b
Manuel Casanova <manucasanova@hotmail.com>
2022-05-16 16:36:19 -0700

    Review function to avoid error found on developer tools

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,55 +1,55 @@
-import React from "react";
-import DayListItem from "components/DayListItem";
-
-export default function DayList(props) {
-
-//const DayListIt = props.DayListItem.map(DayList =>{
-
-  const {days} = props;
-  const DayListIt = days.map(DayList =>{
-
-
-  return (
-    <DayListItem
-      key={DayList.id}
-      name={DayList.name}
-      spots={DayList.spots}
-      selected={DayList.name === props.day}
-      setDay={DayList.setDay}
-      />
-  );
-});
-
-  return (
-    <ul>
-      {DayListIt}
-    </ul>
-  );
-}
-
-//With Ian
-
 // import React from "react";
 // import DayListItem from "components/DayListItem";
 
 // export default function DayList(props) {
 
-//   const {days, day, setDay} = props;
+// //const DayListIt = props.DayListItem.map(DayList =>{
+
+//   const {days} = props;
+//   const DayListIt = days.map(DayList =>{
 
 
 //   return (
+//     <DayListItem
+//       key={DayList.id}
+//       name={DayList.name}
+//       spots={DayList.spots}
+//       selected={DayList.name === props.day}
+//       setDay={DayList.setDay}
+//       />
+//   );
+// });
+
+//   return (
 //     <ul>
-//       {days.map(dayOfWeek =>{
-//         return (
-//           <DayListItem
-//             key={dayOfWeek.id}
-//             name={dayOfWeek.name}
-//             spots={dayOfWeek.spots}
-//             selected={dayOfWeek.name === props.day}
-//             setDay={props.setDay}
-//             />
-//           );
-//       })}
+//       {DayListIt}
 //     </ul>
 //   );
 // }
+
+//With Ian
+
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+
+  const {days, day, setDay} = props;
+
+
+  return (
+    <ul>
+      {days.map(dayOfWeek =>{
+        return (
+          <DayListItem
+            key={dayOfWeek.id}
+            name={dayOfWeek.name}
+            spots={dayOfWeek.spots}
+            selected={dayOfWeek.name === props.day}
+            setDay={props.setDay}
+            />
+          );
+      })}
+    </ul>
+  );
+}
