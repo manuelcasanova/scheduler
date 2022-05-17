ad8265bee3efc911b51effba569d53ca7673cb20
Manuel Casanova <manucasanova@hotmail.com>
2022-05-17 08:07:16 -0700

    Refactor. Do not pass the id prop to the InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,7 +8,10 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
+    //Refactored from
+    //<li onClick={() => props.setInterviewer(props.id)} 
+    <li onClick={props.setInterviewer} 
+    className={interviewerClass}>
   <img
     className="interviewers__item-image"
     src={props.avatar}
