1239ef0546e421cf534c94fbacf8b43617f41498
Manuel Casanova <manucasanova@hotmail.com>
2022-05-16 17:00:09 -0700

    Add classes conditionally. Add the scss elected only when interviewer is selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
+  let interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
+
+
+
+
+
   return (
-    <li onClick={() => props.setInterviewer(props.id)} className="interviewers__item">
+    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
   <img
     className="interviewers__item-image"
     src={props.avatar}
