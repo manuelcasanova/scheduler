028daff4bf1003d361a595da01e350a1396df21a
Manuel Casanova <manucasanova@hotmail.com>
2022-05-16 20:05:07 -0700

    Create and render an array of InterviewerListItem. Include Selected and SetInterviewer function on click

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,14 +1,26 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import "./InterviewerList.scss"
 
-export default function InterviewerList(props) {
+export default function InterviewerList (props) {
 
-
-
+  const intervListIt = props.interviewers.map(interviewer => 
+    
+    <InterviewerListItem 
+      key={interviewer.id}
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+    />
+  )
   return (
-<section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"></ul>
-</section>
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {intervListIt}
+      </ul>
+    </section>
   );
-};
+}
