c7b0aea8d198b80e7a947cdbab746693f3a15907
Manuel Casanova <manucasanova@hotmail.com>
2022-05-18 14:51:00 -0700

    Add moch appointments data. Import components/Appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import "components/Appointment";
+
 
 //Mock days data. Eventyally will be retrieved from an API
 const days = [
@@ -20,6 +22,47 @@ const days = [
     spots: 0,
   },
 ];
+
+//Mock appoitnments data. Eventyally will be retrieved from an API
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 
 
