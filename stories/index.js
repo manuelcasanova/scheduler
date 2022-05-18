259e61ac69257082183cd026d1d6af670c18fd03
Manuel Casanova <manucasanova@hotmail.com>
2022-05-18 10:42:47 -0700

    Add stories for Form component on edit and create

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
+import Form from "components/Appointment/Form.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -161,6 +162,5 @@ storiesOf("DayList", module)
         .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
         .add("Status", () => <Status message="Deleting"/>)
         .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
-        
-
-        
+        .add("Edit", () => <Form name="Manuel Casanova" interviewer={1} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+        .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
