fb807220c6dbe24a2dfa924bcb3028dcc7a1a901
Manuel Casanova <manucasanova@hotmail.com>
2022-05-18 10:54:35 -0700

    Add function to reset forms

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,15 @@ export default function Form(props) {
   const [interviewer, setInterviewer]= useState(props.interviewer || null);
   const [student, setStudent] = useState(props.name || "");
 
+  const reset = function(){
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = function(){
+    reset();
+  }
+
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -19,17 +28,13 @@ export default function Form(props) {
           placeholder="Enter Student Name"
           value={student}
           onChange={(event) => setStudent(event.target.value)}
-
-          /*
-            This must be a controlled component
-          */
          />
         </form>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
         </section>
         <section className="appointment__card-right">
           <section className="appointment__actions">
-            <Button danger onClick={props.onCancel}>Cancel</Button>
+            <Button danger onClick={cancel}>Cancel</Button>
             <Button confirm onClick={props.onSave}>Save</Button>
           </section>
         </section>
