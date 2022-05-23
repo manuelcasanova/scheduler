09d32e931a5982c27b357299112f47e011315a6a
Manuel Casanova <manucasanova@hotmail.com>
2022-05-23 16:09:52 -0700

    Remove appointments object. Add appointments: {} to state. Make dailyAppointments variable and set to empty array. Change the map() function that uses appointments to use dailyAppointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,73 +4,31 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
 
-//Mock appoitnments data. Eventyally will be retrieved from an API
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
 export default function Application(props) {
-  //const [day, setDay] = useState("Monday");
 
-  const [day, setDay] = useState([]);
-  const [days, setDayData] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
-
+  const dailyAppointments = []
   
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
   useEffect(() => {
     const url = 'http://localhost:8001/api/days'
     axios.get(url)
       .then(res => {
-        setDayData(res.data)
+        setDays(res.data)
       })
       .catch(err => console.log(err))
   }, [])
 
+const schedule = dailyAppointments.map((oneAppointment) => {
 
-// console.log(appointments)
-const appointmentsArray = Object.values(appointments);
-// console.log(appointmentsArray);
-
-const schedule = appointmentsArray.map((oneAppointment) => {
-  //console.log("appt", oneAppointment)
   return (
-    // <Appointment key={oneAppointment.id} id={oneAppointment.id} time={oneAppointment.time} interview={oneAppointment.interview} />
     <Appointment 
   key={oneAppointment.id} 
   {...oneAppointment} 
@@ -90,8 +48,8 @@ const schedule = appointmentsArray.map((oneAppointment) => {
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
   <DayList 
-  days={days} 
-  value={day} // refactored from day={day} 
+  days={state.days} 
+  value={state.day} // refactored from day={day} 
   onChange={setDay} /> 
 </nav>
 {/* refactored from "setDay={setDay} /> */}
