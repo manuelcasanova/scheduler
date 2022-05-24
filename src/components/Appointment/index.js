92600994d3ba557f3b7f65fe02a4918be20c158c
Manuel Casanova <manucasanova@hotmail.com>
2022-05-24 14:02:40 -0700

    import useVisualMode. Add mode constants. Use useVisualMode hook SHOW/EMPTY

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import "./styles.scss";
+
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
+import useVisualMode from "hooks/useVisualMode";
+
+
+
 export default function Appointment(props) {
 
+      const EMPTY = "EMPTY";
+      const SHOW = "SHOW";
+      
+      const { mode, transition, back } = useVisualMode(
+          props.interview ? SHOW : EMPTY
+        );
+
 return (
+
+
 <article className="appointment">
-  
 <Header time={props.time}/>
       {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/>: <Empty />}
-
 </article>
-)
 
+
+)
 }
+
