86dd6124c5450b6b28876d42a07b1900d97a0700
Manuel Casanova <manucasanova@hotmail.com>
2022-05-24 14:28:00 -0700

    Update to handle the transition to create. Set the interviewers prop to an empty array because the form component requires a list of interviewers and we have not yet implementer the getInterviewersForDay selector. Use the back function to return to the empty state when cancel button is clicked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import "./styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 
 import useVisualMode from "hooks/useVisualMode";
 
@@ -24,16 +25,23 @@ return (
 
 <article className="appointment">
 <Header time={props.time}/>
-{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+{mode === EMPTY && 
+  <Empty onAdd={() => transition(CREATE)} 
+  />}
+
 {mode === SHOW && (
   <Show
     student={props.interview.student}
     interviewer={props.interview.interviewer}
   />
 )}
+
+{mode === CREATE && 
+  <Form name={props.name} value={props.value} interviewers={[]} onCancel={back}
+/>
+}
 </article>
 
 
-)
-}
+)}
 
