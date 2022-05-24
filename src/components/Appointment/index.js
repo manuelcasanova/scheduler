c99d2ce21746f94f481fa4e35f0e5efaefc74235
Manuel Casanova <manucasanova@hotmail.com>
2022-05-24 14:08:00 -0700

    Add CREATE constant. Update the on Add function to transition to the CREATE mode when the user clicks the add button

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -13,6 +13,7 @@ export default function Appointment(props) {
 
       const EMPTY = "EMPTY";
       const SHOW = "SHOW";
+      const CREATE = "CREATE";
       
       const { mode, transition, back } = useVisualMode(
           props.interview ? SHOW : EMPTY
@@ -23,7 +24,7 @@ return (
 
 <article className="appointment">
 <Header time={props.time}/>
-{mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
 {mode === SHOW && (
   <Show
     student={props.interview.student}
