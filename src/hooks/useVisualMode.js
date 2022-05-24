79f0f018dc2a4f72c24a03f9a160d782a5559b68
Manuel Casanova <manucasanova@hotmail.com>
2022-05-24 09:46:50 -0700

    Add test to develop the transition functionality. Create transition function within useVisualMode. It takes a new mode and updates the mode sate with it.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,17 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  const transition = (newMode, replace = false) => {
+    if (!replace) {
+      setMode(newMode)
+      history.push(newMode)      
+    } else {
+      setMode(newMode)
+    }
+  }
+
+  return { mode, transition };
 }
+
