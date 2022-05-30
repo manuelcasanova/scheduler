acd8cd7224dc8d2b39c415a7cc797a9fce7857ab
Manuel Casanova <manucasanova@hotmail.com>
2022-05-30 09:31:07 -0700

    Add code to update the spots available after booking or cancelling an appointment

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
+import { whichDay } from "helpers/selectors";
 
 export default function useApplicationData() {
 
@@ -28,6 +29,8 @@ export default function useApplicationData() {
     })
   }, [])
 
+
+
   function bookInterview(id, interview) {
     console.log(id, interview);
     const appointment = {
@@ -38,15 +41,39 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
+
+    const weekDay = whichDay(state.day)
+
+    let day = {
+      ...state.days[weekDay],
+      spots: state.days[weekDay]
+    }
+
+    if (!state.appointments[id].interview) {
+      day = {
+        ...state.days[weekDay],
+        spots: state.days[weekDay].spots - 1
+      } 
+    } else {
+      day = {
+        ...state.days[weekDay],
+        spots: state.days[weekDay].spots
+      } 
+    }
+
+    let days = state.days
+    days[weekDay] = day;
+
+
     return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview:interview})
     .then(res => {
-        setState({...state, appointments})
+        setState({...state, appointments, days})
         return res
       })
   }
 
   function cancelInterview(id) {
-    console.log(id);
+    // console.log(id);
     const appointment = {
       ...state.appointments[id],
       interview: null
@@ -55,9 +82,21 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
+
+    const weekDay = whichDay(state.day)
+
+    const day = {
+      ...state.days[weekDay],
+      spots: state.days[weekDay].spots + 1
+    }
+
+    let days = state.days
+    days[weekDay] = day;
+
+
     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
     .then(res => {
-        setState({...state, appointments})
+        setState({...state, appointments, days})
         return res
       })
   }
