b1c20df4392e34f2696b77ebd50a973cf9a5b61d
Manuel Casanova <manucasanova@hotmail.com>
2022-05-29 21:13:08 -0700

    Apply separation of concern. Move code to useApplicationData hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,78 +1,26 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import axios from "axios";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
+const {
+  state,
+  setDay,
+  bookInterview,
+  cancelInterview
+} = useApplicationData();
 
-  //console.log(state.interviewers);
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day) //refactor from (state, state.day) to (state, day) in activity "Retrieving interviewer data"
+  const interviewers = getInterviewersForDay(state, state.day);
 
-  const daysURL = 'http://localhost:8001/api/days';
-  const appointmentsURL = 'http://localhost:8001/api/appointments';
-  const interviewersURL = 'http://localhost:8001/api/interviewers'
-  
-  const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({ ...prev, days }));
+  const schedule = dailyAppointments.map((oneAppointment) => {
+    const interview = getInterview(state, oneAppointment.interview);
 
-  useEffect(() => {
-    Promise.all([
-      axios.get(daysURL),
-      axios.get(appointmentsURL),
-      axios.get(interviewersURL)
-    ]).then((all) => {
-      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data, interviewers:all[2].data}))
-    })
-  }, [])
-
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview:interview})
-    .then(res => {
-        setState({...state, appointments})
-        return res
-      })
-  }
-
-  function cancelInterview(id) {
-    console.log(id);
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
-    .then(res => {
-        setState({...state, appointments})
-        return res
-      })
-  }
-
-const interviewers = getInterviewersForDay(state, state.day);
-
-const schedule = dailyAppointments.map((oneAppointment) => {
-    const interview = getInterview(state, oneAppointment.interview); //Added in in activity "Retrieving interviewer data"
   return (
     <Appointment 
   key={oneAppointment.id} 
@@ -97,7 +45,7 @@ const schedule = dailyAppointments.map((oneAppointment) => {
 <nav className="sidebar__menu">
   <DayList 
   days={state.days} 
-  value={state.day} // refactored from day={day} 
+  value={state.day} 
   onChange={setDay} 
 /> 
 </nav>
