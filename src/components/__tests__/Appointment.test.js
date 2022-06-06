8ec0b763e1a45c9b227cba0938edd3ccb53d1aed
Manuel Casanova <manucasanova@hotmail.com>
2022-06-06 13:27:13 -0700

    Add a few mock tests from the reading 'Mocking'

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -10,6 +10,28 @@ describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
   });
+  
+//Add a few mock tests from the reading "Mocking"
+it("doesn't call the function", () => {
+  const fn = jest.fn();
+  expect(fn).toHaveBeenCalledTimes(0);
+});
+it("calls the function", () => {
+  const fn = jest.fn();
+  fn();
+  expect(fn).toHaveBeenCalledTimes(1);
+ });
+ it("calls the function with specific arguments", () => {
+  const fn = jest.fn();
+  fn(10);
+  expect(fn).toHaveBeenCalledWith(10);
+ });
+ it("uses the mock implementation", () => {
+  const fn = jest.fn((a, b) => 42);
+  fn(1, 2);
+  expect(fn).toHaveReturnedWith(42);
+ });
+
 })
 
 
