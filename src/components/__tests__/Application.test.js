c3ee8554abe2eb53a89862e5c50824e356335386
Manuel Casanova <manucasanova@hotmail.com>
2022-06-09 09:16:20 -0700

    Recover deleted by mistake test. Continue to implement test: load, cancel interview and increase spots

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
-import { getByText, prettyDOM, getByAltText, getByPlaceholderText, getAllByTestId, queryByText } from "@testing-library/react";
+import { getByText, prettyDOM, getByAltText, getByPlaceholderText, getAllByTestId, queryByText, queryByAltText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -26,22 +26,65 @@ describe("Form", () => {
     });
   });
 
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
+  
+    // console.log(prettyDOM(container));
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+  
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+  
+    // console.log(prettyDOM(debug));
+  
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+  
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+  
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+  });
 
+  
   
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
 
     //1 . Render the application.
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     //2. Wait until the text Archie Cohen is displayed
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
   // 3. Click the "Delete" button on the booked appointment.
+  const appointment = getAllByTestId(container, "appointment").find(
+    appointment => queryByText(appointment, "Archie Cohen")
+  );
+
+  fireEvent.click(queryByAltText(appointment, "Delete"));
+
   // 4. Check that the confirmation message is shown.
+  expect(getByText(appointment, "Delete the appointment?")).toBeInTheDocument();
+
   // 5. Click the "Confirm" button on the confirmation.
   // 6. Check that the element with the text "Deleting" is displayed.
   // 7. Wait until the element with the "Add" button is displayed.
   // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+
+
+  //In the solution we have added the debug function, which we destructure from the return function, to the end of our test. This way we can continue to verify that our DOM contains what we expect it to as we go through phase two of our test.
+
+debug();
 
   });
 
