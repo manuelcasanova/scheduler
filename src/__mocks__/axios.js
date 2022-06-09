c92a2a7689aaa0e273bbfb450ca3245df2acf676
Manuel Casanova <manucasanova@hotmail.com>
2022-06-09 08:43:21 -0700

    Modify the url to contain the full address

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -98,7 +98,7 @@ export default {
   defaults: { baseURL: "" },
   get,
   put: jest.fn(url => {
-    if (url === "/api/days") {
+    if (url === "http://localhost:8001/api/days") {
       return Promise.resolve({
         status: 200,
         statusText: "OK",
@@ -106,7 +106,7 @@ export default {
       });
     }
 
-    if (url === "/api/appointments") {
+    if (url.startsWith("http://localhost:8001/api/appointments")) {
       /* Resolve appointments data */
       return Promise.resolve({
         status: 200,
@@ -115,7 +115,7 @@ export default {
       });
     }
 
-    if (url === "/api/interviewers") {
+    if (url === "http://localhost:8001/api/interviewers") {
       /* Resolve interviewers data */
       return Promise.resolve({
         status: 200,
