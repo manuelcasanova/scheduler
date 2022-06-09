074fabbde2551684813dc76b047d67e3c8e918bd
Manuel Casanova <manucasanova@hotmail.com>
2022-06-09 08:49:36 -0700

    Comment in test. Now passing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
-import { getByText, prettyDOM, getByAltText,getByPlaceholderText } from "@testing-library/react";
+import { getByText, prettyDOM, getByAltText,getByPlaceholderText, getAllByTestId, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -16,18 +16,23 @@ afterEach(cleanup);
 
 describe("Form", () => {
 
-  //Test not passing
-// it("defaults to Monday and changes the schedule when a new day is selected", () => {
-//   const { getByText } = render(<Application />);
+  //Test now passing (I changed the http:// in axios.js)
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
 
-//   return waitForElement(() => getByText("Monday")).then(() => {
-//     fireEvent.click(getByText("Tuesday"));
-//     expect(getByText("Leopold Silvers")).toBeInTheDocument();
-//   });
-// });
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+});
 
+
+//Test no passing
 it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-  const { container } = render(<Application />);
+  const { container, debug } = render(<Application />);
+
+  // console.log(prettyDOM(container));
+  
 
   await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -39,11 +44,21 @@ it("loads data, books an interview and reduces the spots remaining for Monday by
   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
     target: { value: "Lydia Miller-Jones" }
   });
-  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
   fireEvent.click(getByText(appointment, "Save"));
 
-  console.log(prettyDOM(appointment));
+  // console.log(prettyDOM(debug));
+
+  expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+  await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+
+  expect(getByText(day, "no spots remaining")).toBeInTheDocument();
 });
 
 });
