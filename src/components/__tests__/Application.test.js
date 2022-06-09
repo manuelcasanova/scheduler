711300b65b3a5a30b3c643712adc81572f1b919e
Manuel Casanova <manucasanova@hotmail.com>
2022-06-09 09:04:19 -0700

    Plan test: load data, cancel interview, increase spots remaining

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
-import { getByText, prettyDOM, getByAltText,getByPlaceholderText, getAllByTestId, queryByText } from "@testing-library/react";
+import { getByText, prettyDOM, getByAltText, getByPlaceholderText, getAllByTestId, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -17,48 +17,33 @@ afterEach(cleanup);
 describe("Form", () => {
 
   //Test now passing (I changed the http:// in axios.js)
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
-});
 
 
-//Test no passing
-it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-  const { container, debug } = render(<Application />);
-
-  // console.log(prettyDOM(container));
   
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
 
-  await waitForElement(() => getByText(container, "Archie Cohen"));
+    //1 . Render the application.
+    const { container } = render(<Application />);
 
-  const appointments = getAllByTestId(container, "appointment");
-  const appointment = appointments[0];
+    //2. Wait until the text Archie Cohen is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
 
-  fireEvent.click(getByAltText(appointment, "Add"));
+    //3. Click on the delete button.
 
-  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-    target: { value: "Lydia Miller-Jones" }
+    //4. Click on the confirm button
+
+    //5. Check that the element with the text "Deleting" is displayed
+
+    //6. Check that the DayListItem with the text Monday has the text "2 spots remaining"
+
   });
-
-  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-  fireEvent.click(getByText(appointment, "Save"));
-
-  // console.log(prettyDOM(debug));
-
-  expect(getByText(appointment, "Saving")).toBeInTheDocument();
-
-  await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
-
-  const day = getAllByTestId(container, "day").find(day =>
-    queryByText(day, "Monday")
-  );
-
-  expect(getByText(day, "no spots remaining")).toBeInTheDocument();
-});
 
 });
